862224518a9de21f78794baff347ad6be65aa2b5
Gyuseok Lee <hahohh@gmail.com>
2022-07-25 21:43:08 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
     hostname     = "app.terraform.io" # default
 
     workspaces {
-      name = "terraform-edu-chapter7-github-action"
+      name = "terraform-edu-chapter7-tfc"
     }
   }
   required_providers {
