d8f8f473891a0583f55cfc8273bd23f37ce8ce4a
“yoojonglee” <yoojonglee@lguplus.co.kr>
2022-10-20 17:12:30 +0900

    hyphen_to_underscore

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+ cloud {
+  organization = "yoojonglee"
+  hostname = "app.terraform.io" # default 
+  workspaces {
+    name = "terraform-edu-chapter7-tfc" 
+  }
+}
+ 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -147,7 +155,7 @@ resource "aws_instance" "hashicat" {
 # Set up some environment variables for our script.
 # Add execute permissions to our scripts.
 # Run the deploy_app.sh script.
-resource "null_resource" "configure-cat-app" {
+resource "null_resource" "configure_cat_app" {
   depends_on = [aws_eip_association.hashicat]
 
   // triggers = {
